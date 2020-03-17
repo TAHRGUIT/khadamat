bcfe78172b307edf813325d976e24b8f7a2b0a11
TAHRGUIT <wmap@live.fr>
2020-03-17 17:57:08 +0100

    update 17/03

--- app/src/main/java/com/yt/khadamat/MainActivity.java
+++ app/src/main/java/com/yt/khadamat/MainActivity.java
@@ -2,13 +2,39 @@ package com.yt.khadamat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    String arrayName [] = {
+            "plumber","electrician","carpenter","mechanic"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CircleMenu circleMenu = findViewById(R.id.circleMenu);
+        circleMenu.setMainMenu(Color.parseColor("#cdcdcd"),R.drawable.menu,R.drawable.close)
+                .addSubMenu(Color.parseColor("#e67e22"),R.drawable.plumber)
+                .addSubMenu(Color.parseColor("#d35400"),R.drawable.electrician)
+                .addSubMenu(Color.parseColor("#e74c3c"),R.drawable.carpenter)
+                .addSubMenu(Color.parseColor("#2c3e50"),R.drawable.mechanic)
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+                    @Override
+                    public void onMenuSelected(int index) {
+                        Toast.makeText(MainActivity.this, arrayName[index], Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
     }
 }
